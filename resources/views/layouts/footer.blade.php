f8dfc36c93d4f8578eec3ca0cdecbbf2c30e3000
ANrajin <dev.quadque@gmail.com>
2021-08-29 17:56:35 +0600

    ui updated

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2021 &copy; <a href="https://quadque.tech">Quadque Technologies LTD</a>
+            </div>
+        </div>
+    </div>
+</footer>
