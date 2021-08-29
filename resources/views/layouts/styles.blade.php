b3b44e7c24989ef150070e75bd4f5285517cbef7
ANrajin <dev.quadque@gmail.com>
2021-08-29 17:32:55 +0600

    updated

---
+++ resources/views/layouts/styles.blade.php
@@ -0,0 +1,7 @@
+<!-- App css -->
+<link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+<link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('assets/css/app.css')}}" rel="stylesheet" type="text/css"  id="app-stylesheet" />
+
+<!-- C3 Chart css -->
+<link href="{{asset('assets/libs/c3/c3.min.css')}}" rel="stylesheet" type="text/css" />
