7d2763323b6d823f5b089e0527c0cf1a2c859591
ANrajin <dev.quadque@gmail.com>
2021-08-31 10:47:39 +0600

    login ui added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,107 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Betler Multipurpose Forms HTML Template">
+    <meta name="author" content="Ansonika">
+    <title>QUADMIN</title>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    <!-- BASE CSS -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/form-vendor.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/form.css')}}" rel="stylesheet">
+</head>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+<body>
+	
+	<div id="preloader">
+		<div data-loader="circle-side"></div>
+	</div><!-- /Preload -->
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+	<div class="container-fluid p-0">
+	    <div class="row no-gutters row-height">
+	        <div class="col-lg-6 background-image " data-background="url({{asset('assets/images/1280_writing-down-goals_4460x4460.jpg')}})">
+	            <div class="content-left-wrapper opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.6)">
+	                <div class="text-left">
+	                	<small>Welcome back</small>
+	                    <h1>Duis aute irure dolor in reprehenderit in voluptate velit esse.</h1>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col-lg-6 d-flex flex-column content-right">
+                <div class="text-center pt-5">
+                    <img src="{{asset('assets/logo.png')}}" alt="">
+                </div>
+	            <div class="container my-auto py-5">
+	                <div class="row">
+	                    <div class="col-lg-9 col-xl-7 mx-auto">
+							@if ($errors->any())
+								<div class="mb-4 text-danger">
+									<div class="font-medium text-red-600">
+										{{ __('Whoops! Something went wrong.') }}
+									</div>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+									<ul class="mt-3 list-disc list-inside text-sm text-red-600">
+										@foreach ($errors->all() as $error)
+											<li>{{ $error }}</li>
+										@endforeach
+									</ul>
+								</div>
+							@endif
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+	                        <form class="input_style_2" method="post" action="{{ route('login') }}">
+								@csrf
+	                            <div class="form-group">
+	                                <label for="email_address">Email Address</label>
+	                                <input type="email" name="email" id="email_address" class="form-control" value="{{old('email')}}">
+	                            </div>
+	                            <div class="form-group">
+	                                <label for="password">Password</label>
+	                                <input type="password" name="password" id="password" class="form-control" required autocomplete="current-password">
+	                            </div>
+	                            <div class="clearfix mb-3">
+	                                <div class="float-left">
+	                                    <label class="container_check">Remember Me
+	                                        <input type="checkbox">
+	                                        <span class="checkmark"></span>
+	                                    </label>
+	                                </div>
+	                                <div class="float-right">
+	                                    <a id="forgot" href="javascript:void(0);">Forgot Password?</a>
+	                                </div>
+	                            </div>
+	                            <button type="submit" class="btn_1 full-width">Login</button>
+	                        </form>
+	                        <form class="input_style_2" method="post">
+	                            <div id="forgot_pw">
+	                                <h4 class="mb-4">Forgot Password</h4>
+	                                <div class="form-group">
+	                                    <label for="email_forgot">Login email</label>
+	                                    <input type="email" class="form-control" name="email_forgot" id="email_forgot">
+	                                </div>
+	                                <p>You will receive an email containing a link allowing you to reset your password to a new preferred one.</p>
+	                                <div class="text-center"><input type="submit" value="Reset Password" class="btn_1"></div>
+	                            </div>
+	                        </form>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="container pb-3 copy">© 2021 Quadque Technologies ltd - All Rights Reserved.</div>
+	        </div>
+	    </div>
+	    <!-- /row -->
+	</div>
+	<!-- COMMON SCRIPTS -->
+    <script src="{{asset('assets/js/common_scripts.js')}}"></script>
+	<script src="{{asset('assets/js/common_func.js')}}"></script>
+</body>
+</html>
