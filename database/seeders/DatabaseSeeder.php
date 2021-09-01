3a7735ff7922fd0e48a14edbe14edcc3deeecb43
ANrajin <dev.quadque@gmail.com>
2021-09-01 11:52:00 +0600

    super admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\UserInfo;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@quadque.com',
+            'password' => Hash::make('admin')
+        ]);
+
+        UserInfo::create([
+            'user_id' => $user->id
+        ]);
     }
 }
