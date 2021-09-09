3a3fec9e9b5ea964cbe94ee5c4de146295a6a58e
ANrajin <dev.quadque@gmail.com>
2021-09-09 13:12:25 +0600

    updated

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -2,10 +2,17 @@
 
 @section('title', 'Edit Categories')
 
+@section('links')
+    <a class="btn btn-sm btn-outline-secondary ml-2" href="{{ route('category.all') }}">
+        <i class="bi bi-arrow-left-circle"></i>&nbsp;
+        Return
+    </a>
+@endsection
+
 @section('content')
-<div class="row">
-    <div class="col-md-6 mx-auto">
-        <update-category category = "{{$category}}" list = "{{$all}}"></update-category>
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <update-category category = "{{$category}}" list = "{{$all}}"></update-category>
+        </div>
     </div>
-</div>
 @endsection
