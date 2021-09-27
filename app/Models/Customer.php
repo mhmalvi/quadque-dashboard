adb8134d28eeb9a13ecb8696e3aa5833c44afeae
ANrajin <dev.quadque@gmail.com>
2021-09-27 15:53:24 +0600

    migration added

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,4 +10,34 @@ class Customer extends Model
     use HasFactory;
 
     protected $table = "users";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
