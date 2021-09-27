9a813d0d9883953c8a0cbcd5c703601657235f22
ANrajin <dev.quadque@gmail.com>
2021-09-27 16:35:49 +0600

    migration updated

---
+++ database/migrations/2021_09_27_102601_add_billing_columns_to_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBillingColumnsToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('order_no');
+            $table->string('payment_type');
+            $table->string('payment_id');
+            $table->string('payer_id');
+            $table->decimal('amount', 8, 2);
+            $table->decimal('coupon_discount', 8, 2)->default(0);
+            $table->enum('payment_status', ['paid', 'unpaid'])->default('unpaid');
+            $table->enum('order_status', ['pending', 'approved', 'canceled'])->default('pending');
+            $table->text('billing_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
