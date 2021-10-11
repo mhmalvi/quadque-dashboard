ec3835dd22fc3627a725fec437404028de8f46d2
ANrajin <dev.quadque@gmail.com>
2021-10-11 13:47:53 +0600

    updated

--- app/Http/Controllers/FeaturedProductsController.php
+++ app/Http/Controllers/FeaturedProductsController.php
@@ -39,4 +39,41 @@ class FeaturedProductsController extends Controller
             'message' => "Successfully updated the featured product's status",
         ], 200);
     }
+
+    // /**
+    //  * Get all products
+    //  */
+    // public function getProducts()
+    // {
+    //     try {
+    //         // if request has category, filter by that category
+    //         if (request()->has('category')) {
+    //             $category = Category::where('category', request()->get('category'))->first();
+
+    //             return new ProductsCollection(
+    //                 $category->products
+    //             );
+    //         }
+    //         // or else, fetch all products
+    //         else {
+    //             return new ProductsCollection(
+    //                 Product::with(['category', 'brand'])
+    //                     ->when(request('search', false), function ($query) {
+    //                         $query->where(function ($q) {
+    //                             $q->where('product', "LIKE", "%" . request('search') . "%")
+    //                                 ->orWhere('price', "LIKE", "%" . request('search') . "%")
+    //                                 ->orWhere('sku', "LIKE", "%" . request('search') . "%");
+    //                         });
+    //                     })
+    //                     ->when(request('category', false), function ($query) {
+    //                         $category = Category::where('slug', request('category'))->first()->id;
+    //                         $query->where('category_id', $category);
+    //                     })
+    //                     ->paginate(request('items'))
+    //             );
+    //         }
+    //     } catch (\Throwable $th) {
+    //         return response()->json(['message' => $th->getMessage()], 503);
+    //     }
+    // }
 }
